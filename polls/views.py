ead81115ebfbbb173e8bbc77d22f89d378a297c6
Daniel Mooney <danielmooney@Daniels-MacBook-Pro.local>
2015-12-07 21:59:12 -0500

    adding comments and goolge map functionality1

--- polls/views.py
+++ polls/views.py
@@ -10,12 +10,16 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import bson
 
+
+#set mongo client to e c2 instance and the collection to tweets for this assingment
 client = MongoClient('ec2-52-27-233-131.us-west-2.compute.amazonaws.com', 27017)
 db = client.users
 dbname = db._Database__name
 collection = db.tweets
 collname = collection.collection._Collection__name
 
+
+#loading the index 
 def index(request):
 	context = None
 	template = loader.get_template('polls/index.html')
@@ -24,9 +28,9 @@ def index(request):
 	next = ''
 	tweetbody = ''
 	totalsize = 0
-	
-	if request.method == 'POST':
 		
+	if request.method == 'POST':
+		#sets variables based on the request post keys	
 		if 'comments' in request.POST.keys():
 			id = request.POST['id']
 			collection.update({'_id':ObjectId(id)},{'$set':{"comment":request.POST['comments']}})
@@ -36,19 +40,23 @@ def index(request):
 		else:
 			next = 0
 		
+		#when username and tweetbody are passed ie search we run mongo query and fetch results
 		if 'username' in request.POST.keys() and 'tweetbody' in request.POST.keys():
 			username = request.POST['username']
 			tweetbody = request.POST['tweetbody']
 
+			
 			limitval = 15
 			results = collection.find({'text':{"$regex":'.*'+str(tweetbody)+'.*'},'fromUser':{"$regex":'.*'+str(username)+'.*'}})
 			
 			totalsize = results.explain()['n']
 			totalpages = totalsize//15 + 1
 			
+			#used skip and limit to limit searches and have pages of results
 			results.skip(next*limitval).limit( limitval )
 			resultsStr += '<table>'
 			
+			#obtain the results and build an html tables to inject into the display tweet when user clicks "View Tweet" div in index.html file
 			for result in results:
 				text     = result['text']
 				text 	 = text.replace("'","\ ")
@@ -64,6 +72,7 @@ def index(request):
 				injectedHTML += '<tr><td>Longitude:</td><td>'+str(result['longitude'])+'</td></tr>'
 				injectedHTML += '<tr><td>Tweeted At:</td><td>'+result['createdAt']+'</td></tr>'
 				
+				#if there is comment then display comment else display nothing
 				if 'comment' in result.keys():
 					comments = result['comment']
 					comments = comments.replace("'","\ ")
@@ -72,27 +81,34 @@ def index(request):
 					comments = ''
 				
 				injectedHTML += '<tr><td>Comments: </td><td>'+comments+'</td></tr>'
+				
+				#button for displaying comment
 				injectedHTML += '<tr><td><button name=&quot;addcomment&quot; type=&quot;submit&quot;>Add Comment</button></td><td><input type=&quot;text&quot; name=&quot;comments&quot; value=&quot; &quot;></td></tr>'
 				injectedHTML += '</table>'
 				injectedHTML += '<button type = &quot;button&quot; onclick=linktogooglemaps(&quot;'+str(result['latitude'])+','+ str(result['longitude'])+'&quot;); class=&quot;buttons&quot;>Google Map Location</button>'
+				
+				#hidden post values 
 				injectedHTML += '<input type=&quot;hidden&quot; name=&quot;id&quot; value=&quot;'+str(result['_id'])+'&quot;/>'
 				injectedHTML += '<input type=&quot;hidden&quot; name=&quot;username&quot; value=&quot;'+username+'&quot;/>'
 				injectedHTML += '<input type=&quot;hidden&quot; name=&quot;tweetbody&quot; value=&quot;'+tweetbody+'&quot; />'
 				injectedHTML += '<input type=&quot;hidden&quot; name=&quot;nextvalue&quot; value=&quot;'+str(next)+'&quot; />'
 								
+				#view tweet button with injected html to propegate to tweetdisplay div onclock
 				button 		  = '''<button type = "" onclick="getElementById('tweetdisplay').innerHTML = ' ''' +injectedHTML+''' ' ;show();">View Tweet</button> '''
-
+				
+				#String that is displayed in result div, view tweet button, fromUser and created AT
 				resultsStr   += "<tr><td>" + button+ "</td><td>" + str(result['fromUser']) + "</td><td>" + str(result['createdAt']) + "</td></tr>"
 				
 				
 				
 			resultsStr += '</table>'
-			
+			#limited the number of search to speed up searches of large query, next and previous go through pages of results
 			if next != 0:
 				resultsStr += '<form id="results" name="resultsform" action="/" method="post"><button name="nextvalue" type="submit" value="'+str(next-1)+'">Previous</button>'
 			if next+1 != totalpages:
 				resultsStr += '<form id="results" name="resultsform" action="/" method="post"><button name="nextvalue" type="submit" value="'+str(next+1)+'">Next</button>'
 			
+			#if totalsize of search was 0 ie no result found show now result otherwise show the numer of page out of all pages
 			if totalsize != 0:
 				resultsStr += '<span>&nbsp;&nbsp;&nbsp;&nbsp;' +str(next+1) + ' out of '+ str(totalpages)+ '</span>'
 			else:
@@ -102,7 +118,7 @@ def index(request):
 			
 			context = RequestContext(request, {'resultsStr': resultsStr,})
 	
-	#injecting the searchbox, prepopulated with text if avaible
+	#injecting the searchbox, prepopulated with text if availble, this is used so that username and tweetbody are persisted through all of our post request unless set
 	searchbox = '''<label for="username">Search by User: </label>
 					<input type="text" id="username" name="username" autocomplete="on" value ="''' 
 	searchbox += str(username)			
@@ -125,10 +141,11 @@ def index(request):
 					<button type = "button" class="buttons" onclick="reload();">Reset</button>'''
 					#<button type = "button" class="buttons" onclick="resetTest()">Reset</button>'''
 	
+	#passing python object parameters to the template file(index)
 	params = {"resultsStr":resultsStr, "client":str(client.address),'searchbox':searchbox,'totalsize':totalsize,'dbname':dbname,'collname':collname}
-
-	if resultsStr == '':
-		return render_to_response("polls/index.html",params, context_instance=RequestContext(request))
-	else:
-		return render_to_response("polls/index.html",params, context_instance=RequestContext(request))
+	
+	#if resultsStr == '':
+	return render_to_response("polls/index.html",params, context_instance=RequestContext(request))
+	#else:
+	#	return render_to_response("polls/index.html",params, context_instance=RequestContext(request))
 
